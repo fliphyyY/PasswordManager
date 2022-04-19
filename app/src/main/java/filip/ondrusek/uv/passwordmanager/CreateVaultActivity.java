0d2a6239d5409ceb793b09002a557fc4c23aea3c
Filip Ondrúšek <fliphyy@ctemplar.com>
2022-04-19 16:07:19 +0200

    create vault Password validation finished

--- app/src/main/java/filip/ondrusek/uv/passwordmanager/CreateVaultActivity.java
+++ app/src/main/java/filip/ondrusek/uv/passwordmanager/CreateVaultActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import net.sqlcipher.database.SQLiteDatabase;
 
+import java.util.Objects;
 import java.util.regex.*;
 import android.app.Activity;
 import android.os.Bundle;
@@ -17,15 +18,9 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class CreateVaultActivity extends AppCompatActivity {
     private EditText masterPassword, masterPasswordAgain;
-    private TextInputLayout textInputLayoutCreate;
-    /*private String regex = "^(?=.*[0-9])"
-            + "(?=.*[a-z])(?=.*[A-Z])"
-            + "(?=.*[@#$%^&+=])"
-            + "(?=\\S+$).{8,20}$";*/
-    String regex = "^(?=.*[0-9])";
-            /*+ "(?=.*[a-z])(?=.*[A-Z])"
-            + "(?=.*[@#$%^&+=])"
-            + "(?=\\S+$).{8,20}$";*/
+    private TextInputLayout textInputLayoutCreate, textInputLayoutCreateAgain;
+    static final int MAX_LENGTH = 8;
+    private String masterPasswordInput, masterPasswordInputAgain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,53 +28,59 @@ public class CreateVaultActivity extends AppCompatActivity {
         masterPassword = findViewById(R.id.masterPasswordCreate);
         masterPasswordAgain = findViewById(R.id.masterPasswordCreateAgain);
         textInputLayoutCreate = findViewById(R.id.textInputLayoutCreate);
+        textInputLayoutCreateAgain = findViewById(R.id.textInputLayoutCreateAgain);
         textInputLayoutCreate.setErrorEnabled(true);
+        textInputLayoutCreateAgain.setErrorEnabled(true);
         masterPassword.setOnFocusChangeListener((v, hasFocus) -> {
             if (!hasFocus) {
                 hideKeyboard(v);
             }
+                Objects.requireNonNull(textInputLayoutCreate.getEditText()).addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void onTextChanged(CharSequence text, int start, int count, int after) {
+                        passwordValidation(text);
+                    }
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    }
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        masterPasswordInput = s.toString();
+                    }
+                });
+
         });
         masterPasswordAgain.setOnFocusChangeListener((v, hasFocus) -> {
             if (!hasFocus) {
                 hideKeyboard(v);
             }
+
+                Objects.requireNonNull(textInputLayoutCreateAgain.getEditText()).addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void onTextChanged(CharSequence text, int start, int count, int after) {
+                        if(!text.toString().equals(masterPasswordInput))
+                        {
+                            textInputLayoutCreateAgain.setError(getString(R.string.match_passwords));
+                            textInputLayoutCreateAgain.setErrorEnabled(true);
+                        } else {
+                            textInputLayoutCreateAgain.setErrorEnabled(false);
+                        }
+                    }
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        masterPasswordInputAgain = s.toString();
+                    }
+                });
         });
         /*String password = "123456";
         VaultDbHelper vaultDbHelper = new VaultDbHelper(getApplicationContext());
         SQLiteDatabase database = vaultDbHelper.getWritableDatabase("123456");
         vaultDbHelper.onDelete(database);*/
-        masterPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                    textInputLayoutCreate.getEditText().addTextChangedListener(new TextWatcher() {
-                        // ...
-                        @Override
-                        public void onTextChanged(CharSequence text, int start, int count, int after) {
-                            if (text.length() > 0 && text.length() <= 8) {
-                                textInputLayoutCreate.setError(getString(R.string.min_length));
-                                textInputLayoutCreate.setErrorEnabled(true);
-                            } else if(!isValidPassword(text.toString())) {
-                                textInputLayoutCreate.setError(getString(R.string.special_password));
-                                textInputLayoutCreate.setErrorEnabled(true);
-                            }
-                            else {
-                                textInputLayoutCreate.setErrorEnabled(false);
-                            }
-                        }
-                        @Override
-                        public void beforeTextChanged(CharSequence s, int start, int count,
-                                                      int after) {
-                            // TODO Auto-generated method stub
-                        }
-
-                        @Override
-                        public void afterTextChanged(Editable s) {
-
-                        }
-                    });
-                }
-
-         });
     }
 
     private void hideKeyboard(View view) {
@@ -87,36 +88,39 @@ public class CreateVaultActivity extends AppCompatActivity {
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
-    private void setupFloatingLabelError() {
-        textInputLayoutCreate.getEditText().addTextChangedListener(new TextWatcher() {
-            // ...
-            @Override
-            public void onTextChanged(CharSequence text, int start, int count, int after) {
-                if (text.length() > 0 && text.length() <= 4) {
-                    textInputLayoutCreate.setError(getString(R.string.app_name));
-                    textInputLayoutCreate.setErrorEnabled(true);
-                } else {
-                    textInputLayoutCreate.setErrorEnabled(false);
-                }
-            }
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+    private void passwordValidation(CharSequence text) {
+        if(isValidPassword(text.toString(), Pattern.compile("[a-z ]"))){
+            textInputLayoutCreate.setError(getString(R.string.letter_lower_password));
+            textInputLayoutCreate.setErrorEnabled(true);
+        }
+        else if(isValidPassword(text.toString(), Pattern.compile("[0-9 ]"))){
+            textInputLayoutCreate.setError(getString(R.string.number_password));
+            textInputLayoutCreate.setErrorEnabled(true);
+        }
+        else if(isValidPassword(text.toString(), Pattern.compile("[A-Z ]"))){
+            textInputLayoutCreate.setError(getString(R.string.letter_upper_password));
+            textInputLayoutCreate.setErrorEnabled(true);
+        }
+        else if(isValidPassword(text.toString(), Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE))) {
+            textInputLayoutCreate.setError(getString(R.string.special_password));
+            textInputLayoutCreate.setErrorEnabled(true);
+        }
+        else if(text.toString().contains(" ")){
+            textInputLayoutCreate.setError(getString(R.string.space_password));
+            textInputLayoutCreate.setErrorEnabled(true);
+        }
+        else if (text.length() > 0 && text.length() <= MAX_LENGTH) {
+            textInputLayoutCreate.setError(getString(R.string.min_length));
+            textInputLayoutCreate.setErrorEnabled(true);
+        }
+        else {
+            textInputLayoutCreate.setErrorEnabled(false);
+        }
     }
 
-    private boolean isValidPassword(String password)
+    private boolean isValidPassword(String password, Pattern pattern)
     {
-        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
-        Matcher m = p.matcher(password);
-        boolean check = m.find();
-        return check;
+        Matcher m = pattern.matcher(password);
+        return !m.find();
     }
 }
