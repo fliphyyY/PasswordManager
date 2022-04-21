9433ee8281e162fa7c5d04d8c11538028b83e644
Filip Ondrúšek <fliphyy@ctemplar.com>
2022-04-21 19:33:04 +0200

    Added headline and save to add item fragment

--- app/src/main/java/filip/ondrusek/uv/passwordmanager/AddItemFragment.java
+++ app/src/main/java/filip/ondrusek/uv/passwordmanager/AddItemFragment.java
@@ -1,9 +1,12 @@
 package filip.ondrusek.uv.passwordmanager;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
@@ -14,8 +17,9 @@ public class AddItemFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private EditText name;
+    private EditText name, username, password, url;
     private TextInputEditText notes;
+    private View view;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -47,6 +51,14 @@ public class AddItemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_item, container, false);
+        view = inflater.inflate(R.layout.fragment_add_item, container, false);
+        name = view.findViewById(R.id.nameAdd);
+        username = view.findViewById(R.id.usernameAdd);
+        password = view.findViewById(R.id.passwordAdd);
+        url = view.findViewById(R.id.urlAdd);
+        notes = view.findViewById(R.id.notesAdd);
+            return inflater.inflate(R.layout.fragment_add_item, container, false);
     }
+
+
 }
