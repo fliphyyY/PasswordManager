a1a18870dce521339c5b5a89915299b816c5d66a
Filip Ondrúšek <fliphyy@ctemplar.com>
2022-04-28 17:26:21 +0200

    delete item in popup implemented

--- app/src/main/java/filip/ondrusek/uv/passwordmanager/VaultAdapter.java
+++ app/src/main/java/filip/ondrusek/uv/passwordmanager/VaultAdapter.java
@@ -2,13 +2,16 @@ package filip.ondrusek.uv.passwordmanager;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class VaultAdapter extends RecyclerView.Adapter<VaultAdapter.VaultViewHolder> {
@@ -25,15 +28,38 @@ public class VaultAdapter extends RecyclerView.Adapter<VaultAdapter.VaultViewHol
         this.onItemClickListener = itemClickListener;
     }
 
-    public class VaultViewHolder extends RecyclerView.ViewHolder {
+    public class VaultViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
         public TextView textView;
         public ImageButton imageButton;
         public VaultViewHolder(@NonNull View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.textviewVaultList);
             imageButton = itemView.findViewById(R.id.imageButton);
+            imageButton.setOnClickListener(this);
             itemView.setTag(this);
             itemView.setOnClickListener(onItemClickListener);
+        }
+
+        @Override
+        public void onClick(View v) {
+            showPopupMenu(v);
+        }
+
+        private void showPopupMenu(View view) {
+            PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+            popupMenu.inflate(R.menu.popup_menu);
+            popupMenu.setOnMenuItemClickListener(this);
+            popupMenu.show();
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_popup_delete:
+                    return true;
+                default:
+                    return false;
+            }
         }
     }
 
